1019c3c7b878bd03d3487e94faaa56307aaec5cc
patrofimov <patrofimov@skbkontur.ru>
2021-08-27 13:33:26 +0500

    logProvider

--- Vostok.Configuration.Logging/ConfigurationProviderSettingsExtensions.cs
+++ Vostok.Configuration.Logging/ConfigurationProviderSettingsExtensions.cs
@@ -8,19 +8,14 @@ namespace Vostok.Configuration.Logging
     [PublicAPI]
     public static class ConfigurationProviderSettingsExtensions
     {
-        /// <summary>
-        /// Enriches <see cref="ConfigurationProviderSettings.ErrorCallback"/> of given <paramref name="settings"/> with exception logging.
-        /// </summary>
-        [NotNull]
-        public static ConfigurationProviderSettings WithErrorLogging([NotNull] this ConfigurationProviderSettings settings, [NotNull] ILog log)
+        /// <inheritdoc cref="WithErrorLogging(ConfigurationProviderSettings,ILog)"/>
+        public static ConfigurationProviderSettings WithErrorLogging([NotNull] this ConfigurationProviderSettings settings, [NotNull] Func<ILog> logProvider)
         {
             if (settings == null)
                 throw new ArgumentNullException(nameof(settings));
 
-            if (log == null)
-                throw new ArgumentNullException(nameof(log));
-
-            log = log.ForContext<ConfigurationProvider>();
+            if (logProvider == null)
+                throw new ArgumentNullException(nameof(logProvider));
 
             var currentCallback = settings.ErrorCallback;
 
@@ -28,16 +23,52 @@ namespace Vostok.Configuration.Logging
             {
                 currentCallback?.Invoke(exception);
 
+                var log = logProvider();
+                if (log == null)
+                    throw new ArgumentNullException(nameof(log));
+
+                log = log.ForContext<ConfigurationProvider>();
+
                 log.Error(exception, "An error has occured in configuration provider.");
             };
 
             return settings;
         }
 
+        /// <summary>
+        /// Enriches <see cref="ConfigurationProviderSettings.ErrorCallback"/> of given <paramref name="settings"/> with exception logging.
+        /// </summary>
+        [NotNull]
+        public static ConfigurationProviderSettings WithErrorLogging([NotNull] this ConfigurationProviderSettings settings, [NotNull] ILog log)
+        {
+            if (log == null)
+                throw new ArgumentNullException(nameof(log));
+
+            return WithErrorLogging(settings, () => log);
+        }
+
+        /// <inheritdoc cref="WithSettingsLogging(ConfigurationProviderSettings,ILog,PrintSettings)"/>
+        [NotNull]
+        public static ConfigurationProviderSettings WithSettingsLogging([NotNull] this ConfigurationProviderSettings settings, [NotNull] Func<ILog> logProvider)
+            => WithSettingsLogging(settings, logProvider, null);
+
         /// <inheritdoc cref="WithSettingsLogging(ConfigurationProviderSettings,ILog,PrintSettings)"/>
         [NotNull]
         public static ConfigurationProviderSettings WithSettingsLogging([NotNull] this ConfigurationProviderSettings settings, [NotNull] ILog log)
             => WithSettingsLogging(settings, log, null);
+
+        /// <inheritdoc cref="WithSettingsLogging(ConfigurationProviderSettings,ILog,PrintSettings)"/>
+        [NotNull]
+        public static ConfigurationProviderSettings WithSettingsLogging(
+            [NotNull] this ConfigurationProviderSettings settings,
+            [NotNull] ILog log,
+            [CanBeNull] PrintSettings printSettings)
+        {
+            if (log == null)
+                throw new ArgumentNullException(nameof(log));
+
+            return WithSettingsLogging(settings, () => log, printSettings);
+        }
 
         /// <summary>
         /// <para>Enriches <see cref="ConfigurationProviderSettings.SettingsCallback"/> of given <paramref name="settings"/> with logging of new settings instances.</para>
@@ -45,17 +76,15 @@ namespace Vostok.Configuration.Logging
         /// </summary>
         [NotNull]
         public static ConfigurationProviderSettings WithSettingsLogging(
-            [NotNull] this ConfigurationProviderSettings settings, 
-            [NotNull] ILog log,
+            [NotNull] this ConfigurationProviderSettings settings,
+            [NotNull] Func<ILog> logProvider,
             [CanBeNull] PrintSettings printSettings)
         {
             if (settings == null)
                 throw new ArgumentNullException(nameof(settings));
 
-            if (log == null)
-                throw new ArgumentNullException(nameof(log));
-
-            log = log.ForContext<ConfigurationProvider>();
+            if (logProvider == null)
+                throw new ArgumentNullException(nameof(logProvider));
 
             var currentCallback = settings.SettingsCallback;
 
@@ -63,8 +92,17 @@ namespace Vostok.Configuration.Logging
             {
                 currentCallback?.Invoke(value, source);
 
-                log.Info("Initialized new settings of type '{SettingsType}' from source of type '{SourceType}': \n{SettingsObject}",
-                    value?.GetType()?.Name, source?.GetType()?.Name, ConfigurationPrinter.Print(value, printSettings));
+                var log = logProvider();
+                if (log == null)
+                    throw new ArgumentNullException(nameof(log));
+
+                log = log.ForContext<ConfigurationProvider>();
+
+                log.Info(
+                    "Initialized new settings of type '{SettingsType}' from source of type '{SourceType}': \n{SettingsObject}",
+                    value?.GetType()?.Name,
+                    source?.GetType()?.Name,
+                    ConfigurationPrinter.Print(value, printSettings));
             };
 
             return settings;
